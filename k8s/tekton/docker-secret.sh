edf3107ba2a49eb5f6353a14b81c22996f6c030c
xiaokang <iishoni@163.com>
2021-08-21 19:57:21 +0800

    修改

---
+++ k8s/tekton/docker-secret.sh
@@ -0,0 +1,6 @@
+kubectl create secret docker-registry docker-secret \
+  --namespace=tekton-pipelines \
+  --docker-server=registry.cn-hangzhou.aliyuncs.com \
+  --docker-username=dxk19911212 \
+  --docker-password=dxk?19911212 \
+  --docker-email=648638730@qq.com
